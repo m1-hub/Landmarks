a892f66656d178ab8602a467486e784d770a8cb4
m1-hub <m-morishita-2q9@eagle.sophia.ac.jp>
2022-08-27 22:13:42 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by etl-13inch-2 on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
